0c9ddb7864beafab5b1eed1f5bc153d165573fd5
JT Yim <JT@JTYim.com>
2015-12-31 01:01:41 -0500

    non working vers- change terminal and base cases

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	if(arguments.length>1){
+		var args = Array.prototype.slice.call(arguments, 1);
+		var x = args[0].childNodes;
+		var x = Array.prototype.filter.call(args[0].childNodes, function(el,i,coll){
+			if(el.nodeName !== '#text' && el.nodeName !== 'SCRIPT'){
+				return el;
+			};
+		});
+		// debugger;
+	}else{
+		var x = Array.prototype.filter.call(document.body.childNodes, function(el,i,coll){
+			if(el.nodeName !== '#text' && el.nodeName !== 'SCRIPT'){
+				return el;
+			};
+		});
+	}
+	// debugger;
+	var y = Array.prototype.forEach.call(x, function(e,i,coll){
+			Array.prototype.forEach.call(e.classList, function(el){
+				debugger;
+				if(el === cl){
+					ans = ans.concat(e);
+				}
+			});
+		if(!(e.childNodes==undefined)){
+			// debugger;
+			getElementsByClassName(cl, e);
+		}else{
+			return;
+		}	
+	});
+	return ans;
+
+	// vs
+	
+	// return document.getElementsByClassName(cl)
 };
